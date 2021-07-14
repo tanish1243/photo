b97d9b04ff33002642b1b278405759704e2140ab
tanish1243 <71543868+tanish1243@users.noreply.github.com>
2021-07-14 16:25:18 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -15,13 +15,21 @@ canvas= createCanvas(600,500);
 function draw() {
 image(video,0,0,600,500);
   tint(filter_color);
-  rect(2, 5, 10, 1000);
+  
+  rect(15, 5, 10, 1000);
+  rect(580, 5, 10, 1000);
+  rect(2, 490, 1000, 10);
+  rect(2, 2, 1000, 10);
+  fill("green")
+  
+  rect(6, 5, 10, 1000);
   rect(590, 5, 10, 1000);
   rect(2, 490, 1000, 10);
   rect(2, 2, 1000, 10);
-
   fill("red");
-  
+
+  circle(40,30,100)
+  circle(570,30,100)
 }
  function applyfilter(){
      filter_color= document.getElementById("filterColourInput").value;
